7b557979422081da109bfcad4598011f65688429
Rebekah MW <81477919+bexwarby@users.noreply.github.com>
2022-06-14 15:43:16 +0200

    refactor: optimised landing by using json

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,6 +8,8 @@ import { Title } from "../components/Heading/Heading"
 import { ImageDisplay as Image } from "../components/Image/Image"
 import { ButtonList as Buttons } from "../components/Button/Button"
 import { Footer } from "../components/Footer/Footer"
+import landing from "../json/landing.json"
+
 
 interface LandingProps {
     title: String;
@@ -22,6 +24,12 @@ const introSx = {
     alignItems: "center"
 }
 
+const introTextSx = {
+    w: { base: 72, sm: "36rem", md: 72, lg: 96 },
+    pl: { base: 0, md: 6 },
+    pt: 4
+}
+
 export const Landing: React.FC<LandingProps> = ({...props}) => {
     return (
         <Box>
@@ -32,12 +40,8 @@ export const Landing: React.FC<LandingProps> = ({...props}) => {
                     src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.18169-9/13043467_10153381032597827_4729354981766043637_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=bPfLqQM83PkAX9Q6X45&_nc_ht=scontent-cdt1-1.xx&oh=00_AT-6LDCmYWY29o0X6EO5sUPL_iPxi3Gld9QipzXwu6HRRg&oe=62C9C263" 
                     alt="Family photo"
                 />
-                <Box w={{base: 72, sm: "36rem", md: 72, lg: 96}} pl={{base: 0, md: 6}}>
-                    <Text>
-                        With over <b>40 years of combined experience</b>, Rebekah and David are a father-daughter duo originating from Leeds and living in Yarm in North Yorkshire. <br/><br/>
-                        They thoroughly love photography and creative writing with a passion.<br/><br/>
-                        Rebekah even dabbles in a spot of dancing in her free time!
-                    </Text>
+                <Box sx={introTextSx}>
+                    <Text dangerouslySetInnerHTML={{__html: landing.introText}}/>
                 </Box>
             </Box>  
             <Buttons />
