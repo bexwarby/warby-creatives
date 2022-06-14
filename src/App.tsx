cbbc6161be737af26b4480f8d4bf25c5cd3e6232
Rebekah MW <81477919+bexwarby@users.noreply.github.com>
2022-06-14 10:21:13 +0200

    feat: added prettier; removed ChakraProvider from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import * as React from "react"
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
 } from "@chakra-ui/react"
 import { Landing } from "./pages/Landing";
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
+    <Box textAlign="center">
       <Landing title="Warby Creations"/>
     </Box>
-  </ChakraProvider>
 )
