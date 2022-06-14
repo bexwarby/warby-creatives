a4588dfc9e549eeba35e49ce4596fa68ba4be951
Rebekah MW <81477919+bexwarby@users.noreply.github.com>
2022-06-14 15:35:34 +0200

    style: fixed mobile design in buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,38 +1,32 @@
 import * as React from "react"
 import {
   Box,
-  Button,
-  ButtonGroup
+  Button
 } from "@chakra-ui/react"
 import landing from "../../json/landing.json"
 import banner from "../../assets/banner.jpg"
 
-interface ButtonProps {
-    background?: string
-}
-
 const wrapperSx = {
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    width:'100%',
-    py:4,
-    bgPosition:'center',
-    bgRepeat:'no-repeat',
-    mb:2
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    py: 4,
+    bgPosition: "center",
+    bgRepeat: "no-repeat",
+    mb: 2
 }
 
-export const ButtonList: React.FC<ButtonProps> = ({...props}) => {
+export const ButtonList: React.FC = () => {
     return (
         <Box sx={wrapperSx} bgImage={banner}>
-            <ButtonGroup gap='4'>
-                {landing.buttons.map(
-                    ({ title, cta, colour }) => 
-                        <Button as="a" href={cta} colorScheme={colour}>
-                            {title}
-                        </Button>
-                )}
-            </ButtonGroup>
+            {landing.buttons.map(({ title, cta, colour }) => 
+                <Button m={8} as="a" href={cta} colorScheme={colour}>
+                    {title}
+                </Button>
+            )}
         </Box>
     )
 }
