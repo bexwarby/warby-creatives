5308b8d36b4c302e0686c5c74cc2df6b516c05ca
Rebekah MW <81477919+bexwarby@users.noreply.github.com>
2022-06-14 09:15:43 +0200

    style: responsive heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -12,10 +12,10 @@ interface HeadingProps {
 export const Title: React.FC<HeadingProps> = ({...props}) => {
     return (
         <Box boxShadow="2xl" bg="green.500" color="white">
-            <Heading as='h1' p={6} size='4xl' noOfLines={1}>
+            <Heading as='h1' p={6} size={{base: 'xl', md: '2xl', lg: '4xl'}} noOfLines={1}>
                 {props.title}
             </Heading>
-            <Heading as='h2' p={4} size='xl' noOfLines={1}>
+            <Heading as='h2' p={4} size={{base: 'md', md: 'xl'}} noOfLines={1}>
                 {props.subtitle}
             </Heading>
         </Box>
